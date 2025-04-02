83a42f149865b5dd77452d5fab43d25b0a0ac379
DESKTOP-AIKJGQ0\gabriel <gabriel.volponi@vidibr.com>
2025-04-01 22:44:27 -0300

    Add Tailwind CSS setup with configuration files and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html", // Inclua o arquivo HTML principal
-    "./**/*.html",  // Inclua outros arquivos HTML no projeto
-    "./**/*.js",    // Inclua arquivos JavaScript, se necessário
-  ],
+  content: ["./index.html"],
   theme: {
     extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'], // Define "Poppins" como fonte primária
-      },
       colors: {
-        primary: '#14DB8F', // Cor primária personalizada
-        secondary: '#BABF15', // Cor secundária personalizada
-        accent: '#72B6F2', // Cor de destaque personalizada
+        primary: {
+          light: '#A7F3D0', // Verde claro
+          DEFAULT: '#34D399', // Verde médio
+          dark: '#065F46', // Verde escuro
+        },
+        secondary: {
+          light: '#FDE68A', // Amarelo claro
+          DEFAULT: '#F59E0B', // Amarelo médio
+          dark: '#B45309', // Amarelo escuro
+        },
+        accent: {
+          light: '#BFDBFE', // Azul claro
+          DEFAULT: '#3B82F6', // Azul médio
+          dark: '#1E3A8A', // Azul escuro
+        },
+      },
+      fontFamily: {
+        nature: ['"Poppins"', 'sans-serif'], // Fonte personalizada
       },
     },
   },
